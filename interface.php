92df8c0e65e4e9c408d1ecd2cd31315bb1f0d142
qinguiz <qinguiz@hotmail.com>
2016-10-02 15:06:13 +0800

    interface

--- interface.php
+++ interface.php
@@ -25,4 +25,14 @@
 	$animal->eat("Banana");
 	//可以用instanceof来判断某个对象是否实现了某个接口
 	var_dump($human instanceof IcanEat);
+
+	//接口可以用extends来继承接口
+	interface IcanPee extends IcanEat{
+		public function pee();
+	}
+
+	class Human2{
+		public function pee(){}
+		public function eat($food){}
+	}
  ?>
