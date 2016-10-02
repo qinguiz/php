5144ad893c43de2332e4f6fb8e8ca93dff93466f
qinguiz <qinguiz@hotmail.com>
2016-10-02 08:59:09 +0800

    destruct

--- demo1.php
+++ demo1.php
@@ -9,7 +9,7 @@
 
 		//构造函数，在对象被实例化时自动调用；
 		function __construct($name,$height,$weight,$team,$playerNumber){
-			echo "IN NbaPlayer construct <br>";
+			echo "IN NbaPlayer construct \n";
 			$this->name = $name;
 			$this->height = $height;
 			$this->weight = $weight;
@@ -17,17 +17,22 @@
 			$this->playerNumber = $playerNumber;
 		}
 
+		//析构函数，在程序执行完之后自动调用；
+		function __destruct(){
+			echo "Destroying ".$this->name."\n";
+		}
+
 		//定义方法
 		public function run(){
-			echo "Running <br>";
+			echo "Running \n";
 		}
 
 		public function jump(){
-			echo "Jumping <br>";
+			echo "Jumping \n";
 		}
 
 		public function dribble(){
-			echo "Dribbling <br>";
+			echo "Dribbling \n";
 		}
 	}
 
@@ -37,9 +42,14 @@
 
 	//类后面的参数列表会作为参数自动调用类里面的构造函数；
 	$jeams = new NbaPlayer("Jeams","203cm","102kg","Heats","6");
+
+	//对象设置为null，就直接调用析构函数
+	// $jeams = null;
+
 	//对象中的属性可以通过 “->” 符号来访问；
-	echo $jordan->name."<br>";
-	echo $jeams->name."<br>";
+	echo $jordan->name."\n";
+
+	echo $jeams->name."\n";
 
 	//对象中的方法可以通过 “->” 符号来访问；
 
