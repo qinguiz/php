6641c9429d5c61a7eebcd5b93d13fc98035de4e2
qinguiz <qinguiz@hotmail.com>
2016-10-02 20:21:32 +0800

    overloading

--- MagicTest.php
+++ MagicTest.php
@@ -8,9 +8,20 @@
 		public function __invoke($param){
 			echo "This is a num ".$param."\n";
 		}
+		//重载方法
+		//__call($name,$arguments);$name时方法名称，$argument是数组。当对象访问不存在的方法名称时，会自动调用
+		public function __call($name,$arguments){
+			echo "Calling ".$name." with parameters: ".implode(",",$arguments)."\n";
+		}
+		//__callStatic($name,$arguments);$name时方法名称，$argument是数组。当对象访问不存在的静态方法时，会自动调用
+		public static function __callStatic($name,$arguments){
+			echo "Calling ".$name." with parameters: ".implode(",",$arguments)."\n";
+		}
 	}
 
 	$var = new MagicTest();
 	echo $var;
 	$var(6);
+	$var->run("para1","para2");
+	MagicTest::run("para1","para2");
  ?>
