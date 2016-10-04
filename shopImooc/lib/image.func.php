917aff0abe5260706a1d073f5247760641756159
qinguiz <qinguiz@hotmail.com>
2016-10-04 10:48:11 +0800

    buildrandomstring

--- shopImooc/lib/image.func.php
+++ shopImooc/lib/image.func.php
@@ -2,12 +2,22 @@
 //通过GD库做验证码
 //创建画布
 $width=80;
-$height=20;
+$height=30;
 $image=imagecreatetruecolor($width, $height);
 $white=imagecolorallocate($image, 255, 255, 255);
 $black=imagecolorallocate($image, 0, 0, 0);
 //用填充矩形填充画布
 imagefilledrectangle($image, 1, 1, $width-2, $height-2, $white);
-
+$chars=buildRandomString($type,$length);
+$sess_name="verify";
+$_SESSION[$sess_name]=$chars;
+for($i=9;$i<length;$i++){
+	$size=mt_rand(14,18);
+	$angle=mt_rand(-15,15);
+	$x=5+$i*$size;
+	$y=mt_rand(20,26);
+	$color=imagecolorallocate($image, mt_rand(0,255), mt_rand(0,255), mt_rand(0,255));
+	imagettftext($image, $size, $angle, x, y, color, fontfile, text)
+}
 
  ?>
--- shopImooc/lib/string.func.php
+++ shopImooc/lib/string.func.php
@@ -1,6 +1,17 @@
 <?php 
-	
-	function buildRandomString(){
-		
+function buildRandomString($type=1,$length=4){
+	if ($type==1) {
+		$chars=join("",range(0,9)); //range(0,9)产生0-9的数组,并选其一
+	}elseif($type==2){
+		$chars=join("",array_merge(range("a", "z"),range("A", "Z")));
+	}elseif($type==3){
+		$chars=join("",array_merge(range("a","z"),range("A", "Z"),range(0, 9)));
 	}
+	if ($length>strlen($chars)) {
+		exit("字符串长度不够");
+	}
+	$chars=str_shuffle($char);
+	return substr($char, 0,$length);
+}
  ?>
+
