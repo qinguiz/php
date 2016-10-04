8c1a8b5612f3f5b81edd2f830eb787e95eb470c7
qinguiz <qinguiz@hotmail.com>
2016-10-04 13:42:37 +0800

    verify

--- shopImooc/lib/image.func.php
+++ shopImooc/lib/image.func.php
@@ -1,34 +1,46 @@
 <?php 
 require_once "string.func.php";
+function verifyImage($type=1,$length=4,$sess_name="verify"){
 //通过GD库做验证码
 //创建画布
 $width=80;
 $height=30;
 $image=imagecreatetruecolor($width,$height);
-$white=imagecolorallocate($image, 255, 255, 255);
-$black=imagecolorallocate($image, 0, 0, 0);
+$white=imagecolorallocate($image,255,255,255);
+$black=imagecolorallocate($image,0,0,0);
 //用填充矩形填充画布
-imagefilledrectangle($image, 1, 1, $width-2, $height-2, $white);
-$type=1;
-$length=4;
+imagefilledrectangle($image,1,1,$width-2,$height-2,$white);
 $chars=buildRandomString($type,$length);
-$sess_name="verify";
+// $sess_name="verify";
 $_SESSION[$sess_name]=$chars;
-$fontfiles=array("方正粗谭黑简体.ttf","简准圆.ttf");
-for($i=0;$i<length;$i++){
+$fontfiles=array("fz.ttf","zy.ttf");
+for($i=0;$i<$length;$i++){
 	$size=mt_rand(14,18);
 	$angle=mt_rand(-15,15);
 	$x=5+$i*$size;
 	$y=mt_rand(20,26);
 	$fontfile="../fonts/".$fontfiles[mt_rand(0,count($fontfiles)-1)];
-	$color=imagecolorallocate($image, mt_rand(0,255), mt_rand(0,255), mt_rand(0,255));
-	$text=substr($char, $i,1);
-	imagettftext($image, $size, $angle, $x, $y, $color, $fontfile, $text);
+	$color=imagecolorallocate($image,mt_rand(0,255),mt_rand(0,255),mt_rand(0,255));
+	$text=substr($chars,$i,1);
+	imagettftext($image,$size,$angle,$x,$y,$color,$fontfile,$text);
 }
-
-
+// $pixel = 50;
+// if ($pixel) {
+// 	for($i=0;$i<50;$++){
+// 		imagesetpixel($image, mt_rand(0,$width-1), mt_rand(0,$height-1), $black);
+// 	}
+// }
+// $line = 10;
+// if ($line) {
+// 	for($i = 1;$i<$line;$i++){
+// 		$color = imagecolorallocate($image, mt_rand(50,90), mt_rand(80,200), mt_rand(50,90));
+// 		imageline($image, mt_rand(0,$width-1), mt_rand(0,$height-1), x2, y2, color)
+// 	}
+// }
+ob_clean();
 header("content-type:image/gif");
 imagegif($image);
 imagedestroy($image);
-
+}
+verifyImage();
  ?>
--- shopImooc/lib/string.func.php
+++ shopImooc/lib/string.func.php
@@ -1,7 +1,7 @@
 <?php 
 function buildRandomString($type=1,$length=4){
 	if ($type==1) {
-		$chars=join("",range(0,9)); //range(0,9)产生0-9的数组,并选其一
+		$chars=join("",range(0,9)); //range(0,9)产生0-9的数组
 	}elseif($type==2){
 		$chars=join("",array_merge(range("a", "z"),range("A", "Z")));
 	}elseif($type==3){
@@ -10,8 +10,8 @@ function buildRandomString($type=1,$length=4){
 	if ($length>strlen($chars)) {
 		exit("字符串长度不够");
 	}
-	$chars=str_shuffle($char);
-	return substr($char, 0,$length);
+	$chars=str_shuffle($chars);
+	return substr($chars, 0,$length);
 }
  ?>
 
